ca546751af2a2d054c55925c1a08398789045db8
pr454d-p <7697prasad@gmail.com>
2022-09-22 07:57:50 +0530

    final draft

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm'
+import remarkBreaks from 'remark-breaks'
+import './style.css'
 function App() {
+  const [text,setText] = useState(`
+  # H1
+  ## H2
+  **bold text**
+  > blockquote
+  1. First item
+  2. Second item
+  3. Third item
+  \`code\`
+  [title](https://www.example.com)
+  ![alt text](image.jpg)
+  \`\`\`
+  {
+    "firstName": "John",
+    "lastName": "Smith",
+    "age": 25
+  }
+  \`\`\` 
+  `);
+
+  const markdown = text;
+  console.log(text)
+  const trsformed = <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm,remarkBreaks]} />
+  console.log(trsformed);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='editor-left' > 
+        <textarea id="editor" value={text} onChange={(words)=>setText(words.target.value)} > </textarea>        
+      </div>
+      <div className='preview-right' id="preview"> 
+      {trsformed}
+      </div>
     </div>
   );
 }
